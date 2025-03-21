d1812b86a617bed0cc4798184a56168ee1671248
RaJah239 <ravijagroo239@gmail.com>
2025-03-21 15:13:56 -0400

    Deleted all unreferenced text in maps

--- maps/ManiasHouse.asm
+++ maps/ManiasHouse.asm
@@ -84,20 +84,11 @@ ManiaScript:
 	closetext
 	end
 
-.nothingleft ; unreferenced
-	writetext ManiaText_ShuckleIsYourLastMon
-	waitbutton
-	closetext
-	end
-
 .default_postevent
 	writetext ManiaText_HappinessSpeech
 	waitbutton
 	closetext
 	end
-
-ManiasHouseUnusedBookshelf: ; unreferenced
-	jumpstd PictureBookshelfScript
 
 ManiaText_AskLookAfterShuckle:
 	text "I, I'm in shock!"
@@ -188,14 +179,6 @@ ManiaText_HappinessSpeech:
 
 	para "with a person who"
 	line "treats them well."
-	done
-
-ManiaText_ShuckleIsYourLastMon:
-	text "If I take my #-"
-	line "MON back, what are"
-
-	para "you going to use"
-	line "in battle?"
 	done
 
 ManiasHouse_MapEvents:
