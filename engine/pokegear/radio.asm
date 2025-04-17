d4bda974e64a1e898c6537abff38ee326f6e5e7d
RaJah239 <ravijagroo239@gmail.com>
2025-04-17 10:00:52 -0400

    Added a new radio channel https://github.com/pret/pokecrystal/wiki/Add-a-new-radio-channel

--- engine/pokegear/radio.asm
+++ engine/pokegear/radio.asm
@@ -32,6 +32,7 @@ RadioJumptable:
 	dw PokeFluteRadio    ; $08
 	dw UnownRadio        ; $09
 	dw EvolutionRadio    ; $0a
+	dw NationalPark      ; $0b
 	assert_table_length NUM_RADIO_CHANNELS
 ; OaksPKMNTalk
 	dw OaksPKMNTalk2     ; $0b
@@ -1417,6 +1418,12 @@ UnownRadio:
 	ret
 
 EvolutionRadio:
+	call StartRadioStation
+	ld a, 1
+	ld [wNumRadioLinesPrinted], a
+	ret
+
+NationalPark:
 	call StartRadioStation
 	ld a, 1
 	ld [wNumRadioLinesPrinted], a
