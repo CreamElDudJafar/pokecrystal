ed4ee5632b484f49306fe5343eff9d900ed395a5
CreamElDudJafar <jahern2398@gmail.com>
2026-03-26 12:39:48 -0500

    Update Bug Contenst Mons

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -1,13 +1,13 @@
 ContestMons:
 	;   %, species,   min, max
-	db 20, CATERPIE,    7, 18
-	db 20, WEEDLE,      7, 18
-	db 10, METAPOD,     9, 18
-	db 10, KAKUNA,      9, 18
-	db  5, BUTTERFREE, 12, 15
-	db  5, BEEDRILL,   12, 15
-	db 10, VENONAT,    10, 16
-	db 10, PARAS,      10, 17
-	db  5, SCYTHER,    13, 14
-	db  5, PINSIR,     13, 14
-	db -1, VENOMOTH,   30, 40
+	db  9, PINECO,     30, 31
+	db 10, YANMA,      30, 31
+	db  9, LEDIAN,     30, 31
+	db  9, BEEDRILL,   30, 31
+	db  9, PARASECT,   30, 31
+	db  9, BUTTERFREE, 30, 31
+	db  9, ARIADOS,    30, 31
+	db  9, PINSIR,     30, 31
+	db  9, SCYTHER,    30, 31
+	db  9, VENOMOTH,   30, 31
+	db  9, HERACROSS,  30, 31
