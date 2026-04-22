RaJah239/Nayru62-s-Dex-Newbox

Assemble Debug ROM with `make d` Debug ROM updates: Shorten intro Interact with Player's Room radio to get a team of Pokémon, set many flags and warp to Cherrygrove City Press A on Egg Stat Screen to hatch any egg after a singe step
Access `DEBUG ROOM` on Continue screen after creating a Save File
Note: Ensure `BATTLE SKIP` in `DEBUG ROOM` is set to `DO` and not `SKIP` as to not OHKO all enemies (if needed)

Credit: https://github.com/SoupPotato/Sourcrystal for the following:
Hold `A` to walk through walls
Hold `A` to ignore trainers
Hold `A` to disable wild encounters

data/default_options.asm
@@ -1,6 +1,11 @@
 DefaultOptions:
-; wOptions: med text speed
+; wOptions: fast text speed
+if DEF(_DEBUG)
+; wOptions: instal text speed
+	db TEXT_DELAY_NONE
+else
 	db TEXT_DELAY_FAST
+endc
 ; wSaveFileExists: no
 	db FALSE
 ; wTextboxFrame: frame 1

CreamElDudJafar/pokecrystal

data/default_options.asm
@@ -1,6 +1,11 @@
 DefaultOptions:
-; wOptions: med text speed
+; wOptions: fast text speed
+if DEF(_DEBUG)
+; wOptions: instal text speed
+	db TEXT_DELAY_FAST
+else
 	db TEXT_DELAY_MED
+endc
 ; wSaveFileExists: no
 	db FALSE
 ; wTextboxFrame: frame 1